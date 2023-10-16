879bbd13952b5e2cdc7182e1072869fab6ea8792
Yordi984 <135142211+Yordi984@users.noreply.github.com>
2023-10-16 11:21:10 -0600

    Delete html 1 directory

--- html 1/script.js
+++
@@ -1,9 +0,0 @@
- 
- const texto = document.getElementById("text")
- fetch("html.txt")
- .then((res) => res.text())
- .then(text=> {
-text = text.replace(/(\`\*\*|\*\*`)/g,"")
-    console.log(text)
-    texto.textContent=text;
- })
--- html 1/yo.py
+++
@@ -1,6 +0,0 @@
-import calendar 
-yy= 2023 #year
-mm=10 #month
-
-#display the calendar 
-print(calendar.month(yy,mm))
